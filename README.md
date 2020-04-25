e58211251af9b17d6368fb860d0ad2d0560568e0
Randy Elzinga <rjelzinga@gmail.com>
2020-04-25 09:12:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# lita-doubler
+
+[![Build Status](https://travis-ci.org/rjelzinga/lita-doubler.png?branch=master)](https://travis-ci.org/rjelzinga/lita-doubler)
+[![Coverage Status](https://coveralls.io/repos/rjelzinga/lita-doubler/badge.png)](https://coveralls.io/r/rjelzinga/lita-doubler)
+
+TODO: Add a description of the plugin.
+
+## Installation
+
+Add lita-doubler to your Lita instance's Gemfile:
+
+``` ruby
+gem "lita-doubler"
+```
+
+## Configuration
+
+TODO: Describe any configuration attributes the plugin exposes.
+
+## Usage
+
+TODO: Describe the plugin's features and how to use them.
+# lita-doubler-on-thi-day
---
+++ spec/lita/handlers/doubler_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe Lita::Handlers::Doubler, lita_handler: true do
+  describe 'routing' do
+    # allow extra white space
+    it { is_expected.to route('Lita double 2') }
+    it { is_expected.to route('Lita double     22') }
+    # allow mixed casing
+    it { is_expected.to route('Lita doUble 4') }
+
+    # ignore numbers that don't look like integers
+    it { is_expected.not_to route('Lita double two') }
+    it { is_expected.not_to route('Lita double 1e4') }
+  end
+
+  describe ':double_number' do
+    let(:n) { rand(1..100) }
+    it 'returns double the input' do
+      actual = subject.double_number(n)
+      expected = n * 2
+
+      expect(actual).to eq(expected)
+    end
+  end
+
+  it 'doubles numbers when asked to' do
+    send_message 'Lita double 2'
+    expect(replies.last).to eq('2 + 2 = 4')
+  end
+end
